8b892a4fe13a983698d004fb625669654e7999d2
Joaco Valenverguen <llerenaluiseduardo3@gmail.com>
2021-04-07 17:19:39 -0500

    env agregado ahora si

--- modo.js
+++ modo.js
@@ -1,3 +1,4 @@
+require('dotenv').config()
 var moodpossible  = ['activo' , 'de prueba'];
 var activo = moodpossible[1];
 var prueba = moodpossible[2];
@@ -5,10 +6,10 @@ var a = 'activo'
 var b = 'prueba'
 var modo = a
 if(modo === 'activo'){
-    var token ='NzU5MTUzNjgxMzYxNzk3MTQw.X25W9g.x7JPcl6T80KCz8UkRR6CuMektr0'
+    var token =process.env.Token1
 }
 else if(modo === 'prueba'){
-    var token = 'NzY1OTY2MTAwNzU2MTY4NzA2.X4cfhQ.lIpAn1dKee3oVossu4loiPNmHwE'
+    var token = process.env.Token2
 }
 module.exports = {
     mode : modo,
